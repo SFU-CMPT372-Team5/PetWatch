b1505d92c4d04d1e80a5c9d4d837cf5ba5700e13
Jodhan Gill <jga145@sfu.ca>
2023-07-12 13:38:06 -0700

    Added and integrated pet creation api endpoint

--- pages/pets/new.vue
+++ pages/pets/new.vue
@@ -1,110 +1,104 @@
 <template>
-    <v-container class="form-container">
-      <h2 class="text-center">Add Pet Form</h2>
-      <br>
-      <v-form @submit.prevent="submitForm">
-        <v-row>
-          <v-col cols="12" sm="6">
-            <v-text-field
-              v-model="name"
-              label="Name"
-              required
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-text-field
-              v-model="age"
-              label="Age"
-              required
-            ></v-text-field>
-          </v-col>
-          
-        </v-row>
-  
-        <v-row>
-          <v-col cols="12" sm="6">
-            <v-text-field
-              v-model="petType"
-              label="Pet Type"
-              required
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12" sm="6">
-            <v-text-field
-              v-model="petColor"
-              label="Pet Color"
-              required
-            ></v-text-field>
-          </v-col>
-           <v-col cols="12" sm="6">
-            <v-text-field
-              v-model="breed"
-              label="Breed"
-              required
-            ></v-text-field>
-          </v-col>
-          
-        </v-row>
+  <v-container class="form-container">
+    <h2 class="text-center">Add Pet Form</h2>
+    <br>
+    <v-form @submit.prevent="submitForm">
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="name" label="Name" required></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="age" label="Age" required></v-text-field>
+        </v-col>
+
+      </v-row>
+
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="petType" label="Pet Type" required></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="petColor" label="Pet Color" required></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field v-model="breed" label="Breed" required></v-text-field>
+        </v-col>
+
+      </v-row>
       <v-container>
-          <v-select
-          v-model="lostStatus"
-          :items="options"
-          label="Is the pet Lost"
-          ></v-select>
-       </v-container>
-        
-        
+        <v-select v-model="lostStatus" :items="options" label="Is the pet Lost"></v-select>
+      </v-container>
+
+
+
+      <v-btn type="submit" color="primary">Submit</v-btn>
+    </v-form>
+  </v-container>
+</template>
   
-        <v-btn type="submit" color="primary">Submit</v-btn>
-      </v-form>
-    </v-container>
-  </template>
-  
-  <script>
-  
-  
-  export default {
-    data() {
-      return {
-        name: '',
-        age: '',
-        petType: '',
-        petColor: '',
-        breed: '',
-        lostStatus: null,
-        options: [
-          'Lost',
-          'Not Lost',
-        ]
-  
-      };
-    },
-    methods: {
-      submitForm() {
-        // Navigate to the FormData page and pass the form data as query parameters
-        this.$router.push({
-          path: '/petInfo',
-          query: {
-            name: this.name,
-            age: this.age,
-            petType: this.petType,
-            petColor: this.petColor,
-            breed: this.breed,
-            lostStatus: this.lostStatus
-           
+<script>
+definePageMeta({ middleware: 'auth' })
+
+const { data } = useAuth()
+
+export default {
+
+  data() {
+    return {
+      data: data,
+      name: '',
+      age: '',
+      petType: '',
+      petColor: '',
+      breed: '',
+      lostStatus: null,
+      options: [
+        'Lost',
+        'Not Lost',
+      ]
+
+    };
+  },
+  methods: {
+    async submitForm() {
+      await $fetch("/api/auth/userID").then(async (response) => {
+        const Pet_UID = "PET" + Date.now()
+        await $fetch("/api/pets/create", {
+          method: 'POST',
+          body: {
+            Pet_UID: Pet_UID,
+            petDetails: {
+              petOwner: response,
+              name: this.name,
+              animalType: this.petType,
+              colour: this.petColor,
+              animalBreed: this.breed
+            },
+            isMissing: this.lostStatus === 'Lost'
           }
-        });
-      }
-    },
-    
+        }).catch(e => {
+          console.error(e)
+        }).then(() => {
+          this.$router.push({
+            //Navigate to the FormData page and pass Pet_UID as query parameter
+            path: '/pets/',
+            query: {
+              petID: Pet_UID,
+            }
+          })
+        }
+        )
+      }).catch(e => {
+        console.error(e)
+      })
+    }
+  }, 
+};
+</script>
   
-  
-  };
-  </script>
-  
-  <style>
-  .form-container {
-    max-width: 400px;
-    margin: 0 auto;
-  }
-  </style>
+<style>
+.form-container {
+  max-width: 400px;
+  margin: 0 auto;
+}
+</style>
