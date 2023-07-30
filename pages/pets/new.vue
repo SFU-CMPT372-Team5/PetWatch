9d4396d292be916a1aae5fe02c497dc8cd8fde20
Divya Soneji <d.soneji@fugro.com>
2023-07-29 19:44:14 -0700

    Improve Pet Creation Workflow

--- pages/pets/new.vue
+++ pages/pets/new.vue
@@ -1,38 +1,35 @@
 <template>
-  <v-container class="form-container">
-    <h2 class="text-center">Add Pet Form</h2>
-    <br>
-    <v-form @submit.prevent="submitForm">
-      <v-row>
-        <v-col cols="12" sm="6">
-          <v-text-field v-model="name" label="Name" required></v-text-field>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <v-text-field v-model="age" label="Age" required></v-text-field>
-        </v-col>
-
-      </v-row>
-
-      <v-row>
-        <v-col cols="12" sm="6">
-          <v-text-field v-model="petType" label="Pet Type" required></v-text-field>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <v-text-field v-model="petColor" label="Pet Color" required></v-text-field>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <v-text-field v-model="breed" label="Breed" required></v-text-field>
-        </v-col>
-
-      </v-row>
-      <v-container>
-        <v-select v-model="lostStatus" :items="options" label="Is the pet Lost"></v-select>
+  <v-container class="bg-pink-lighten-3 fill-height" fluid style="flex-direction: column; transition: background-color 0.3s linear;">
+    <v-card class="ma-5">
+      <v-container style="width: 400px;">
+        <h2 class="text-center">Add Pet Form</h2>
+        <br>
+        <v-form @submit.prevent="submitForm">
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="name" label="Name" required></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="age" label="Age" required></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="petType" label="Pet Type" required></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="petColor" label="Pet Color" required></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="breed" label="Breed" required></v-text-field>
+            </v-col>
+          </v-row>
+          <v-select v-model="lostStatus" :items="options" label="Is the pet Lost"></v-select>
+          <v-btn color="error" class="mr-2" @click="navigateTo('/account')">Cancel</v-btn>
+          <v-btn :disabled="!validForm" type="submit" color="primary">Submit</v-btn>
+        </v-form>
       </v-container>
-
-
-
-      <v-btn type="submit" color="primary">Submit</v-btn>
-    </v-form>
+    </v-card>
   </v-container>
 </template>
   
@@ -45,6 +42,7 @@ export default {
 
   data() {
     return {
+      validForm: false,
       data: data,
       name: '',
       age: '',
@@ -59,6 +57,16 @@ export default {
 
     };
   },
+
+  watch: {
+    name: 'checkFormValidity',
+    age: 'checkFormValidity',
+    petType: 'checkFormValidity',
+    petColor: 'checkFormValidity',
+    breed: 'checkFormValidity',
+    lostStatus: 'checkFormValidity',
+  },
+
   methods: {
     async submitForm() {
       await $fetch("/api/account/info").then(async (response) => {
@@ -82,21 +90,24 @@ export default {
         }).then(() => {
           this.$router.push({
             //Navigate to the FormData page and pass Pet_UID as query parameter
-            path: '/pets/'+Pet_UID
+            path: '/pets/' + Pet_UID
           })
         }
         )
       }).catch(e => {
         console.error(e)
       })
-    }
-  }, 
+    },
+    checkFormValidity() {
+      // Check if all required fields are filled and lostStatus is selected
+      this.validForm = !!this.name && !!this.age && !!this.petType && !!this.petColor && !!this.breed && !!this.lostStatus;
+    },
+  },
+
+  mounted() {
+    // Call the checkFormValidity method on component mount to set the initial state of validForm
+    this.checkFormValidity();
+  },
 };
 </script>
   
-<style>
-.form-container {
-  max-width: 400px;
-  margin: 0 auto;
-}
-</style>
