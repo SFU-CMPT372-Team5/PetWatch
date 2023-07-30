63f7668641ae798997c1ff4e77d554a7ec261d92
Bea <1123bmac@gmail.com>
2023-07-30 00:21:01 -0700

    Add page loader component

---
+++ components/shared/PageLoading.vue
@@ -0,0 +1,7 @@
+<template>
+    <VContainer fluid class="fill-height">
+        <VRow justify="center" align="center" >
+            <VProgressCircular indeterminate color="grey-lighten-4" size="large" />
+        </VRow>
+    </VContainer>
+</template>
