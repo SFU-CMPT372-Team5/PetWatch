d0992f5e0c3f3a3baf482ae61ab6f0cf45092aca
Bea <1123bmac@gmail.com>
2023-07-30 01:01:06 -0700

    Clarify page loading states and unify page load spinner

--- components/shared/PageLoading.vue
+++ components/shared/PageLoading.vue
@@ -1,7 +1,5 @@
 <template>
-    <VContainer fluid class="fill-height">
-        <VRow justify="center" align="center" >
-            <VProgressCircular indeterminate color="grey-lighten-4" size="large" />
-        </VRow>
-    </VContainer>
+    <VRow justify="center" align="center" class="fill-height">
+        <VProgressCircular indeterminate color="grey-lighten-4" size="large" />
+    </VRow>
 </template>
