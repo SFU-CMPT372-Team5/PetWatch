5f890ac3b4562dbfcb72254eed2b085b71dd29d6
MonishkaG <gautam.monishka@gmail.com>
2023-08-01 11:28:11 -0700

    chat start email notif

--- server/api/pet/[petID]/chat/create.post.ts
+++ server/api/pet/[petID]/chat/create.post.ts
@@ -1,9 +1,14 @@
 import { getToken } from '#auth'
 import { chat, pet } from '../../../../mongo/models';
 
+
+import sgMail from '@sendgrid/mail'
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY1!);
+
 export default defineEventHandler(async (event) => {
     const token = await getToken({event}); //The { } are important!
-
+    const body = await readBody(event)
     if (token?.sub != undefined) {
         const existingChat = await chat.findOne({
             petID: event.context.params.petID,
@@ -25,6 +30,21 @@ export default defineEventHandler(async (event) => {
             });
             
             if (newChat != undefined) {
+                const msg = {
+                    to:  body.userDetails.email, // Change to your recipient
+                    from: 'olivia0001002@gmail.com', // Change to your verified sender
+                    subject: 'PetWatch Chat Initiated',
+                    text: 'A pet finder would like to chat with you, please check your account for messages.',
+                    html: '<strong>A pet finder would like to chat with you, please check your account for messages..</strong>',
+                }
+                sgMail
+                .send(msg)
+                .then(() => {
+                    console.log('Email sent')
+                })
+                .catch((error: any) => {
+                    console.error(error)
+                })
                 return newChat
             } else {
                 setResponseStatus(event, 500);
