f2d932e0749c23ff13db2ffef22f5a28dfd92e3c
Bea <1123bmac@gmail.com>
2023-08-01 14:38:30 -0700

    block owner details on found page when not logged in

--- pages/pets/found/[[petID]]/index.vue
+++ pages/pets/found/[[petID]]/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <v-sheet elevation="12" width="100vw" class="pa-4 text-center mx-auto fill-height" :color="(!pending && !found) || petData?.isMissing ? 'red-accent-1' : 'blue-accent-1'" style="transition: background-color 1s linear;">
+    <v-sheet elevation="12" width="100vw" class="pa-4 text-center mx-auto fill-height" :color="(!petPending && petData == undefined) || petData?.isMissing ? 'red-accent-1' : 'blue-accent-1'" style="transition: background-color 1s linear;">
         <VFadeTransition group>
-            <template v-if="pending">
+            <template v-if="petPending">
                 <VRow justify="center" align="center" class="fill-height">
                     <VCol>
                         <VProgressCircular indeterminate color="grey-lighten-4" size="large"/>
@@ -10,7 +10,7 @@
             </template>
     
             <template v-else>
-                <template v-if="found">
+                <template v-if="petData != undefined">
                     <template v-if="petData?.isMissing">
                         <VCard class="mb-1" width="100%">
                             <VRow style="width: 100%">
@@ -48,7 +48,13 @@
                                         <VCard height="100%">
                                             <VCardTitle><h3 class=text-center>Owner Contact Details</h3></VCardTitle>
                                             <VCardText>
-                                                <ContactDetails :data="contactData"/>
+                                                <template v-if="contactError?.status == 403 || contactError?.statusCode == 401">
+                                                    <VCard style="align-self: center;" class="mt-5 ml-5 mr-5" variant="outlined" color="error">
+                                                        <VIcon color="error" size="xxx-large">mdi-close-circle-outline</VIcon>
+                                                        <p>Sign in to view contact details</p>
+                                                    </VCard>
+                                                </template>
+                                                <ContactDetails v-else :data="contactData!"/>
                                             </VCardText>
                                         </VCard>
                                         </VCol>
@@ -102,8 +108,14 @@
                         <VCol :cols="cardWidthCols">
                             <VCard>
                             <v-icon class="mb-5" color="error" icon="mdi-close-circle-outline" size="112"></v-icon>
-                            <VCardTitle>Hmm, that pet wasn't found in our database</VCardTitle>
-                            <VCardText style="font-size: large;">If this is your pet, please make sure the QR code is fully visible and free from damage</VCardText>
+                            <template v-if="$route.params.petID?.length === 0">
+                                <VCardTitle>That URL is invalid</VCardTitle>
+                                <VCardSubtitle>No petID found</VCardSubtitle>
+                            </template>
+                            <template v-else>
+                                <VCardTitle>Hmm, that pet wasn't found in our database</VCardTitle>
+                                <VCardText style="font-size: large;">If this is your pet, please make sure the QR code is fully visible and free from damage</VCardText>
+                            </template>
                             <VCardActions style="justify-content: center;">
 
                                 <VBtn v-if="!loggedIn" 
@@ -128,53 +140,51 @@
     </v-sheet>
 </template>
 
-<script setup lang="ts">
-const PLACEHOLDER_IMAGE_URL = "/images/paw.jpg";
-
-const route = useRoute();
-
-const limitedDataRes = await useLazyFetch<LimitedPetModel|PetModel>(`/api/pet/${route.params.petID}/limitedData`);
-const contactDataRes = await useLazyFetch(`/api/pet/${route.params.petID}/contactDetails`);
-const contactData = contactDataRes.data;
-const petData = limitedDataRes.data;
-const pending = limitedDataRes.pending;
-
-const session = await getSession();
-
-let found = false;
-const loggedIn = session.user != undefined;
-
-watch(limitedDataRes.pending, async (dataVal) => {
-    if (dataVal == true) return;
-
-    if (limitedDataRes.error.value != undefined) return;
-    found = true;
-
-    const {data: amOwner, error} = await useFetch<{owner: boolean}>(`/api/pet/${route.params.petID}/amOwner`)
-
-    if (amOwner.value?.owner) {
-        useRouter().push(`/pets/${route.params.petID}`);
-        return;
-    }
-})
-
-const petImageUrl = computed(() => {
-    return petData.value?.imageURL ?? PLACEHOLDER_IMAGE_URL
-})
-
-</script>
-
 <script lang="ts">
 import PetDetails from "~/components/petProfile/PetDetails.vue"
 import ContactDetails from "~/components/petProfile/ContactDetails.vue";
 import type PetModel from "~/types/models/pet"
+import type { UserDetails } from "~/types/models/user";
 import { type LimitedPetModel } from "~/types/models/pet";
 import ChatLauncher from "~/components/foundPet/ChatLauncher.vue"
 const { getSession} = useAuth();
 
 export default {
     components: {PetDetails, ContactDetails, ChatLauncher},
+    async setup() {
+        const PLACEHOLDER_IMAGE_URL = "/images/paw.jpg";
+
+        const route = useRoute();
+
+        const {data: petData, pending: petPending, error: petError} = await useLazyFetch<LimitedPetModel|PetModel>(`/api/pet/${route.params.petID}/limitedData`);
+
+        const {data: contactData, error: contactError} = await useLazyFetch<UserDetails>(`/api/pet/${route.params.petID}/ownerDetails`);
+
+        const session = await getSession();
+
+        const loggedIn = session.user != undefined;
+
+        watch(petPending, async (dataVal) => {
+            if (dataVal == true) return; //Should be false when no longer pending
+
+            if (petError.value != undefined) return;
+            //If the owner is the current user, just redirect to the non-found pet profile
+            const {data: amOwner, error} = await useFetch<{owner: boolean}>(`/api/pet/${route.params.petID}/amOwner`)
+
+            if (amOwner.value?.owner) {
+                useRouter().push(`/pets/${route.params.petID}`);
+                return;
+            }
+        })
+
+        return {
+            PLACEHOLDER_IMAGE_URL, loggedIn, petData, petPending, contactData, contactError
+        }
+    },
     computed: {
+        petImageUrl() {
+            return this.petData?.imageURL ?? this.PLACEHOLDER_IMAGE_URL
+        },
         colWidths() {
             return this.$vuetify.display.smAndDown ? [12, 12] : [5, 7]
         },
--- types/models/user.d.ts
+++ types/models/user.d.ts
@@ -1,18 +1,18 @@
 export default interface UserModel {
     User_UID: string, //Primary Key
     dateCreated: string,
-    userDetails: {
-        name: str,
-        address: string,
-        phone: string,
-        email: string,
-        additionalInfo: [ //Optional
-            {
-                detailName: String,
-                detailValue: String
-            }
-        ]
-    },
-    pets: string[],
-    nonOwnerPetChats: string[] //A list of chats this person is in for pets that are not their own
+    userDetails: UserDetails
+}
+
+export interface UserDetails {
+    name: string,
+    address: string,
+    phone: string,
+    email: string,
+    additionalInfo: [ //Optional
+        {
+            detailName: String,
+            detailValue: String
+        }
+    ]
 }
