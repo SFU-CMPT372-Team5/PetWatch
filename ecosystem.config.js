fb21126a0c977c8bb2bde94890769496b6771339
Bea <1123bmac@gmail.com>
2023-08-22 19:27:44 -0700

    add ecosystem file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+// For configuring the PM2 process manager
+module.exports = {
+    apps: [
+        {
+            name: "PetWatch",
+            port: "10002",
+            script: "npm",
+            args: "preview"
+        }
+    ]
+}
